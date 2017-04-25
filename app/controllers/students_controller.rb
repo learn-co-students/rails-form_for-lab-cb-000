50b92e9a55b36aa311cb8ff4a9743ae5fede76e0
John Knotts <jhnnyk@gmail.com>
2017-04-24 18:09:13 -0600

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,34 @@
+class StudentsController < ApplicationController
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(post_params(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(post_params(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end
+
+  private
+
+  def post_params(*args)
+    params.require(:student).permit(*args)
+  end
+
+
+end

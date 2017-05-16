fe4022a00057985adcacf162e2b6c0a1b6c5d6db
jwald1 <email@example.com>
2017-05-16 13:00:43 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,26 @@
 class StudentsController < ApplicationController
+  def new
+    @student = Student.new
+  end
+
+  def show
+    @student = Student.find_by(id: params[:id])
+  end
+
+  def edit
+    @student = Student.find_by(id: params[:id])
+  end
+
   def create
     student = Student.new(params_values(:first_name, :last_name))
+    student.save
+
+    redirect_to student
+  end
+
+  def update
+    student = Student.find_by(id: params[:id])
+    student.update(params_values(:first_name, :last_name))
     student.save
 
     redirect_to student
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :school_classes, only: [:create, :show]
-  resources :students, only: [:create, :show]
+  resources :school_classes, only: [:create, :show, :new, :edit, :update]
+  resources :students, only: [:create, :show, :new, :show, :edit, :update]
 end

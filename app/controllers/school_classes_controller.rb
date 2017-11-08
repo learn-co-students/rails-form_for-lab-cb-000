612b728f2bc61296b1c88af7a3dd9d6bc0999583
sjmoody <steven@beachheadmarketing.com>
2017-11-08 16:09:56 +0100

    passing school class features

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -17,10 +17,9 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
-    @school_class = SchoolClass.new
-    @school_class.title = params[:title]
-    @school_class.room_number = params[:room_number]
+    @school_class = SchoolClass.new(params.require(:school_class))
     @school_class.save
+
     redirect_to school_class_path(@school_class)
   end
 
@@ -29,6 +28,9 @@ class SchoolClassesController < ApplicationController
   end
 
   def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(params.require(:school_class))
+    redirect_to school_class_path(@school_class)
   end
 
 

31b1d006e024a5e45b13cf0c449539fb0c3617d0
sjmoody <steven@beachheadmarketing.com>
2017-11-08 15:31:57 +0100

    passing controller and model tests

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -16,9 +16,10 @@ class SchoolClassesController < ApplicationController
 
   def create
     @school_class = SchoolClass.new
-    @school_class.first_name = params[:first_name]
-    @school_class.last_name = params[:last_name]
+    @school_class.title = params[:title]
+    @school_class.room_number = params[:room_number]
     @school_class.save
+    redirect_to school_class_path(@school_class)
   end
 
   def edit
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,12 +16,13 @@ class StudentsController < ApplicationController
     @student.first_name = params[:first_name]
     @student.last_name = params[:last_name]
     @student.save
+    redirect_to student_path(@student)
   end
 
   def edit
   end
 
-  def update 
+  def update
   end
 
 

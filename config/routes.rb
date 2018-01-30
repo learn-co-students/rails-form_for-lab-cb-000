a8dfcf4fa947ae4cc31069c216e90d9ad0012162
Michael Anderson <michanders@msn.com>
2018-01-30 21:33:27 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,12 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  resources :students, only: [:index, :show, :new, :create]
+  get "students/:id/edit", to: "students#edit", as: :edit_student
+  patch "students/:id", to: "students#update"
+
+  resources :school_classes, only: [:index, :show, :new, :create]
+  get "school_classes/:id/edit", to: "school_classes#edit", as: :edit_school_class
+  patch "school_classes/:id", to: "school_classes#update"
 end

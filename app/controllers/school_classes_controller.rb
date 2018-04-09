fcf77ac7283883fabe6eb3cdc64450d645a2e42e
adam n <adam.nawroc@gmail.com>
2018-04-09 17:09:39 +0000

    progress

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,5 +1,18 @@
 class SchoolClassesController < ApplicationController
 
+  def new
 
+  end
+
+def create
+  @schoolclass = SchoolClass.new(schoolclass_params(:title, :number))
+  @schoolclass.save
+  redirect_to schoolclass_path(@schoolclass)
+end
+
+
+def schoolclass_params(*args)
+  params.require(:schoolclass).permit(*args)
+end
 
 end

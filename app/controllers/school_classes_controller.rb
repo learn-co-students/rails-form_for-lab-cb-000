f33fa689ea3db0e4cff68527d6993455690e967b
adam n <adam.nawroc@gmail.com>
2018-04-09 17:25:52 +0000

    progress

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -17,11 +17,25 @@ class SchoolClassesController < ApplicationController
 
 	end
 
+
+
 def create
   @schoolclass = SchoolClass.new(schoolclass_params(:title, :number))
   @schoolclass.save
   redirect_to school_class_path(@schoolclass)
 end
+
+def update
+	  # @post = Post.find(params[:id])
+    @schoolclass = SchoolClass.find(params[:id])
+
+	  # @post.update(title: params[:title], description: params[:description])
+    @schoolclass.update(schoolclass_params(:title, :number))
+    #
+	  # redirect_to post_path(@post)
+    redirect_to school_class_path(@schoolclass)
+
+	end
 
 
 def schoolclass_params(*args)

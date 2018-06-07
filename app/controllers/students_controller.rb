ec247dfbdfe108d468d7f6c040c1ea64a6ca4db2
“Alyssa <clairaly13@gmail.comgit config --global user.name clairaly>
2018-06-07 15:09:01 -0400

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,30 @@
+class StudentsController < ApplicationController
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(student_params)
+    redirect_to student_path(@student)
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:last_name, :first_name)
+  end
+end

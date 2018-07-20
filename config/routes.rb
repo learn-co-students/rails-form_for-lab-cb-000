747eac5878ca37c4a4377398989dfc577a0ffa2a
nichia <nichia@gmail.com>
2018-07-20 09:16:36 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,30 @@
 Rails.application.routes.draw do
+  # resources :students, only: [:index, :show, :new, :create, :edit, :update]
+  # Students routes
+  get 'students', to:'students#index'
+
+  get 'students/new', to: 'students#new', as: 'new_student'
+
+  post 'students', to: 'students#create'
+
+  get 'students/:id', to: 'students#show', as: 'student'
+
+  get 'students/:id/edit', to: 'students#edit', as: 'edit_student'
+
+  patch 'students/:id', to: 'students#update'
+
+  # SchoolClasses routes
+  get 'school_classes', to:'school_classes#index'
+
+  get 'school_classes/new', to: 'school_classes#new', as: 'new_school_class'
+
+  post 'school_classes', to: 'school_classes#create'
+
+  get 'school_classes/:id', to: 'school_classes#show', as: 'school_class'
+
+  get 'school_classes/:id/edit', to: 'school_classes#edit', as: 'edit_school_class'
+
+  patch 'school_classes/:id', to: 'school_classes#update'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

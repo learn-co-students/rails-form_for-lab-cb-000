c8ec6fea53b2b42f564d287bf90da4215a3733df
Felice Forby <feliceforby@gmail.com>
2018-10-03 22:30:28 +0000

    stub out controllers

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,3 +1,24 @@
 class SchoolClassesController < ApplicationController
+  before_action :set_school_class, only: [:show, :edit]
 
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def set_school_class
+    @school_class = SchoolClass.find(params[:id])
+  end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,23 @@
 class StudentsController < ApplicationController
-  
+  before_action :set_student, only: [:show, :edit]
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
 end

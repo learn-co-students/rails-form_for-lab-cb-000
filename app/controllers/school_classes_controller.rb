56f091f42f3c4cce643b0b3b8e17f66e0d292f53
Felice Forby <feliceforby@gmail.com>
2018-10-03 23:19:46 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,24 +1,34 @@
 class SchoolClassesController < ApplicationController
-  before_action :set_school_class, only: [:show, :edit]
+  before_action :set_school_class, only: [:show, :edit, :update]
 
   def show
   end
 
   def new
+    @school_class = SchoolClass.new
   end
 
   def create
+    @school_class = SchoolClass.new(post_params)
+    @school_class.save
+    redirect_to school_class_path(@school_class)
   end
 
   def edit
   end
 
   def update
+    @school_class.update(post_params)
+    redirect_to school_class_path(@school_class)
   end
 
   private
 
   def set_school_class
     @school_class = SchoolClass.find(params[:id])
+  end
+
+  def post_params
+    params.require(:school_class).permit(:title, :room_number)
   end
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,23 +1,33 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: [:show, :edit]
+  before_action :set_student, only: [:show, :edit, :update]
   def show
   end
 
   def new
+    @student = Student.new
   end
 
   def create
+    @student = Student.new(post_params)
+    @student.save
+    redirect_to student_path(@student)
   end
 
   def edit
   end
 
   def update
+    @student.update(post_params)
+    redirect_to student_path(@student)
   end
 
   private
 
   def set_student
     @student = Student.find(params[:id])
+  end
+
+  def post_params
+    params.require(:student).permit(:first_name, :last_name)
   end
 end

69d967fe07b3e0ee67fbee9fe29e2e2f4160d444
Fer Hernandez <fh.1111@gmail.com>
2019-01-12 18:27:33 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :students, except: [:destroy, :index]
+  resources :school_classes, except: [:destroy, :index]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

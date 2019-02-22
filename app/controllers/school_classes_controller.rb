e8754b06f07f2b3ed4332777a0b5cf7e156c9601
Sergio Marrocoli <s.marrocoli@gmail.com>
2019-02-22 18:15:55 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -4,20 +4,31 @@ class SchoolClassesController < ApplicationController
   # protect_from_forgery with: :exception
 
   def index
+    @school_classes = SchoolClass.all
+
   end
 
   def new
   end
 
   def create
+    @school_class =  SchoolClass.create(title: params[:school_class_title], room_number: params[:school_class_room_number].to_i)
+    redirect_to school_class_path(@school_class)
   end
 
   def show
+    @school_class =  SchoolClass.find(params[:id])
   end
 
   def edit
+    @school_class =  SchoolClass.find(params[:id])
   end
 
   def update
+    # binding.pry
+    @school_class =  SchoolClass.find(params[:id])
+    @school_class.update(params.require(:school_class))
+    @school_class.save
+    redirect_to school_class_path(@school_class)
   end
 end

6055eaa059e20b8421140e328adf038c801ad640
Sergio Marrocoli <s.marrocoli@gmail.com>
2019-02-22 18:00:57 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -14,6 +14,8 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @student = Student.create(first_name: params[:student_first_name], last_name: params[:student_last_name])
+    redirect_to student_path(@student)
   end
 
   def show
@@ -28,6 +30,8 @@ class StudentsController < ApplicationController
     # binding.pry
     @student = Student.find(params[:id])
     @student.update(params.require(:student))
+    @student.save
+    redirect_to student_path(@student)
   end
 
 end

96472111bc7c7af3fed391eff1e5fccdc38db966
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-01 10:43:45 +0000

    Done.

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,2 +1,29 @@
 class SchoolClassesController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @school_class = SchoolClass.new(school_class_params(:title, :room_number))
+    @school_class.save
+    redirect_to school_class_path(@school_class)
+  end
+
+  def show
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(school_class_params(:title, :room_number))
+    redirect_to school_class_path(@school_class)
+  end
+
+  def school_class_params(*args)
+    params.require(:school_class).permit(*args)
+  end
+
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,31 @@
 class StudentsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    @student = Student.new(student_params(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+    @student.update(student_params(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end
+
+  def update
+  end
+
+  private
+
+  def student_params(*args)
+    params.require(:student).permit(*args)
+  end
+
 end

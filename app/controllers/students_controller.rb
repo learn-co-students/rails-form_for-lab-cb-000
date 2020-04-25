c4a02ae62557eec244ee115d9bd481e0c3b9bb6d
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-04-25 21:10:21 +0000

    Done.

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,23 @@
+class StudentsController < ApplicationController 
+  
+  def show 
+    
+  end 
+  
+  def new 
+    
+  end 
+  
+  def edit 
+    
+  end 
+  
+  def create
+    
+  end 
+  
+  def update
+    
+  end 
+  
+end 
---
+++ db/migrate/002_create_school_classes.rb
@@ -0,0 +1,7 @@
+class CreateSchoolClasses < ActiveRecord::Migration
+  create_table :school_classes do |t|
+    t.string :title 
+    t.integer :room_number
+  end 
+end
+end 

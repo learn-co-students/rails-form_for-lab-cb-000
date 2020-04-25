77a2eccfa8a4fd49055274e2a7aa3cc470f1012c
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-04-25 23:00:57 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,23 +1,27 @@
 class StudentsController < ApplicationController 
   
   def show 
-    
+    @student = Student.find(params[:id])
   end 
   
-  def new 
-    
-  end 
-  
-  def edit 
-    
+   def new 
+    @student = Student.new
   end 
   
   def create
-    
+        @student = Student.new(params.require(:student))
+        @student.save
+        redirect_to student_path(@student)
+  end 
+  
+    def edit 
+    @student = student.find(params[:id])
   end 
   
   def update
-    
+        @student = student.find(params[:id])
+        @student.update(params.require(:student))
+    @student = student.find(params[:id])
   end 
   
 end 
